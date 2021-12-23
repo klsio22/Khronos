cc9c3751e7d3e2ec6c3d2e3f16ca9e23ff53d59a
klesio Nascimento <klesio.dev@gmail.com>
2021-12-23 18:20:28 -0300

    Added  discover-stores , buy-by-brands and our styles

--- assets/libraries/owl-js/setup.js
+++ assets/libraries/owl-js/setup.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
+
+  $(".owl-brands").owlCarousel({
+    loop: false,
+    nav: true,
+    dots: false,
+    margin: 1,
+    responsive: {
+      0: {
+        
+        items: 1,
+      },
+      568: {
+     
+        items: 3,
+      },
+
+      768: {
+     
+        items: 4,
+      },
+
+
+      1000: {
+        items: 5,
+      },
+
+      1200:{
+        items: 6,
+      }
+    },
+  });
+
+
   $(".owl-carousel").owlCarousel({
     loop: false,
     nav: true,
@@ -19,4 +52,8 @@ $(document).ready(function () {
       },
     },
   });
+
+
+
+
 });
