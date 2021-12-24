c2c9f2c132d3ef447e6144dc47ac47ed13da58d4
klesio Nascimento <klesio.dev@gmail.com>
2021-12-23 22:41:05 -0300

    Created monitoring , brands , our stores, categorys

--- assets/libraries/owl-js/setup.js
+++ assets/libraries/owl-js/setup.js
@@ -1,5 +1,51 @@
 $(document).ready(function () {
 
+
+$(".owl-categorys").owlCarousel({
+    loop: false,
+    nav: true,
+    dots: false,
+    margin: 10,
+    responsive: {
+      0: {
+        
+        items: 3,
+      },
+      
+      758: {
+        
+        items: 5,
+      },
+
+      1000: {
+        
+        items: 10,
+      },
+    },
+  });
+
+ 
+  $(".product-list").owlCarousel({
+    loop: false,
+    nav: true,
+    dots: false,
+    margin: 10,
+    responsive: {
+      0: {
+        nav: false,
+        items: 1,
+      },
+      568: {
+        nav: false,
+        items: 2,
+      },
+
+      1000: {
+        items: 4,
+      },
+    },
+  });
+
   $(".owl-brands").owlCarousel({
     loop: false,
     nav: true,
@@ -26,34 +72,10 @@ $(document).ready(function () {
       },
 
       1200:{
+        
         items: 6,
       }
     },
   });
-
-
-  $(".owl-carousel").owlCarousel({
-    loop: false,
-    nav: true,
-    dots: false,
-    margin: 10,
-    responsive: {
-      0: {
-        nav: false,
-        items: 1,
-      },
-      568: {
-        nav: false,
-        items: 2,
-      },
-
-      1000: {
-        items: 4,
-      },
-    },
-  });
-
-
-
 
 });
