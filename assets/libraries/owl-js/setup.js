00b1777747fb22d9be01580e98a14ac90201cc41
klesio Nascimento <klesio.dev@gmail.com>
2021-12-24 01:03:47 -0300

    changes spaces

--- assets/libraries/owl-js/setup.js
+++ assets/libraries/owl-js/setup.js
@@ -1,30 +1,25 @@
 $(document).ready(function () {
 
-
-$(".owl-categorys").owlCarousel({
+  $(".owl-categorys").owlCarousel({
     loop: false,
     nav: true,
     dots: false,
     margin: 10,
     responsive: {
       0: {
-        
         items: 3,
       },
-      
+
       758: {
-        
         items: 5,
       },
 
       1000: {
-        
         items: 10,
       },
     },
   });
 
- 
   $(".product-list").owlCarousel({
     loop: false,
     nav: true,
@@ -57,29 +52,24 @@ $(".owl-categorys").owlCarousel({
     margin: 1,
     responsive: {
       0: {
-        
         items: 1,
       },
       568: {
-     
         items: 3,
       },
 
       768: {
-     
         items: 4,
       },
-
 
       1000: {
         items: 5,
       },
 
-      1200:{
-        
+      1200: {
         items: 6,
-      }
+      },
     },
   });
-
+  
 });
